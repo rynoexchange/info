abca9c712caca5c58673c0bcea9aebc23b7edd3d
Yada Tashy <88944220+yadatashy@users.noreply.github.com>
2021-09-14 23:12:31 +0300

    Add trustwallet asset support to token logos

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import PlaceHolder from '../../assets/placeholder.png'
 import { useListedTokens } from '../../contexts/Application';
+import { isAddress } from '../../utils'
 
 const BAD_IMAGES = {}
 
@@ -14,16 +15,17 @@ const Inline = styled.div`
 const Image = styled.img`
   width: ${({ size }) => size};
   height: ${({ size }) => size};
-  background-color: white;
   border-radius: 50%;
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
+  background-color: ${({ theme }) => theme.bg1};
 `
 
 export default function TokenLogo({ address, header = false, size = '24px', ...rest }) {
   const [error, setError] = useState(false)
 
   const tokens = useListedTokens();
-  const path = tokens?.full?.find(i => i.address === address)?.logoURI;
+  const path = tokens?.full?.find(i => i.address === address)?.logoURI
+    || `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/poa/assets/${isAddress(address)}/logo.png`;
 
   useEffect(() => {
     setError(false)
